e067f2cd198cfe9fd0c5c8fecd20fc95051cf1e3
Nikhil Kumar Menaria <nikhilkumar.menaria@dxc.com>
2020-11-06 14:06:05 +0530

    Add project files.

---
+++ CRUD/Models/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD.Models
+{
+    public class Student
+    {   [Key]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage ="Required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        public string fname { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        [Display(Name ="Department")]
+        public int DeptID { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        public string Mobile { get; set; }
+
+        public string Description { get; set; }
+
+        [NotMapped]
+        public string Department { get; set; }
+    }
+}
